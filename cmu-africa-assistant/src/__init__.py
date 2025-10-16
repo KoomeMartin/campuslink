bb36fca205bfbb0e1eb97a6bd0d6f7a1c72e72f3
Martin Koome <karimimartin43@gmail.com>
2025-10-16 05:34:42 +0200

    update

--- cmu-africa-assistant/src/__init__.py
+++
@@ -1,3 +0,0 @@
-
-# CMU-Africa Information Assistant Package
-__version__ = "1.0.0"
--- cmu-africa-assistant/src/components/__init__.py
+++
@@ -1,7 +0,0 @@
-
-"""Core components for CMU-Africa Information Assistant"""
-from .rag_pipeline import RAGPipeline
-from .chat_interface import ChatInterface
-from .feedback_handler import FeedbackHandler
-
-__all__ = ['RAGPipeline', 'ChatInterface', 'FeedbackHandler']
